7f5ae4fcb9319e189a3c039414eee866e51cb4ca
zLyricx <ryutamashizin@gmail.com>
2026-04-24 22:48:00 +0000

    criando bot

---
+++ package.json/index.js
@@ -0,0 +1,56 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+      GatewayIntentBits.Guilds,
+          GatewayIntentBits.GuildMessages,
+              GatewayIntentBits.MessageContent
+                ]
+                });
+
+                const banco = {};
+
+                function criarConta(id) {
+                  if (!banco[id]) banco[id] = { nuques: 0 };
+                  }
+
+                  function converter(nuques) {
+                    let galeoes = Math.floor(nuques / (17 * 29));
+                      let resto = nuques % (17 * 29);
+                        let sicles = Math.floor(resto / 29);
+                          let n = resto % 29;
+
+                            return `${galeoes}G ${sicles}S ${n}N`;
+                            }
+
+                            client.on("messageCreate", (msg) => {
+                              if (msg.author.bot) return;
+
+                                const args = msg.content.split(" ");
+                                  const cmd = args[0];
+
+                                    criarConta(msg.author.id);
+
+                                      if (cmd === "!saldo") {
+                                          msg.reply(`💰 ${converter(banco[msg.author.id].nuques)}`);
+                                            }
+
+                                              if (cmd === "!add") {
+                                                  let valor = parseInt(args[1]);
+                                                      banco[msg.author.id].nuques += valor;
+                                                          msg.reply("Adicionado!");
+                                                            }
+
+                                                              if (cmd === "!comprar") {
+                                                                  let preco = parseInt(args[1]);
+
+                                                                      if (banco[msg.author.id].nuques >= preco) {
+                                                                            banco[msg.author.id].nuques -= preco;
+                                                                                  msg.reply("Compra feita!");
+                                                                                      } else {
+                                                                                            msg.reply("Dinheiro insuficiente!");
+                                                                                                }
+                                                                                                  }
+                                                                                                  });
+
+                                                                                                  client.login(process.env.TOKEN);
